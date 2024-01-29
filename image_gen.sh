43126db6e2748c449e8b2c943ad604330cd8ace4
Vitaly Voloshin <vloshinvitaly@yandex.com>
2024-01-29 23:45:09 +0000

    fixing variable name in inmage_gen script

--- image_gen.sh
+++ image_gen.sh
@@ -2,7 +2,7 @@
 set -ex
 
 IMAGE_NAME=${1:-cl_bootcamp_base}
-IMAGE_FILE=${2:-$IMAGENAME_$(date +%Y%m%d)_$(date +"%H%M%S").wasm}
+IMAGE_FILE=${2:-$IMAGE_NAME_$(date +%Y%m%d)_$(date +"%H%M%S").wasm}
 DOCKERFILEPATH=${1:-/$IMAGE_NAME}
 docker build --rm -t $IMAGE_NAME -f ./dockerfiles/$DOCKERFILEPATH .
 ./c2w $IMAGE_NAME:latest $IMAGE_FILE
