efd829f90b07fe4e8c447b5d6b3ca50258b5c3d9
Vitaly Voloshin <voloshinvitaly@yandex.com>
2024-01-29 12:08:31 +0000

    Fixing paths

--- image_gen.sh
+++ image_gen.sh
@@ -4,6 +4,6 @@ set -ex
 IMAGE_NAME=${1:-cl_bootcamp_base}
 IMAGE_FILE=${2:-$IMAGENAME_$(date +%Y%m%d)_$(date +"%H%M%S").wasm}
 DOCKERFILEPATH=${1:-/$IMAGE_NAME}
-docker build --rm -t $IMAGE_NAME -f .dockerfiles/$DOCKERFILEPATH .
+docker build --rm -t $IMAGE_NAME -f ./dockerfiles/$DOCKERFILEPATH .
 ./c2w $IMAGENAME:latest $IMAGENAME.wasm
 sed -i 's/cl-bootcamp-base.wasm/$IMAGENAME.wasm/g' index.html
