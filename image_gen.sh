59a9921955a6953bb78e85ddb24a0b831a124034
Vitaly Voloshin <voloshinvitaly@yandex.com>
2024-01-29 12:29:01 +0000

    Fixing yet another issue with filenames.

--- image_gen.sh
+++ image_gen.sh
@@ -5,5 +5,5 @@ IMAGE_NAME=${1:-cl_bootcamp_base}
 IMAGE_FILE=${2:-$IMAGENAME_$(date +%Y%m%d)_$(date +"%H%M%S").wasm}
 DOCKERFILEPATH=${1:-/$IMAGE_NAME}
 docker build --rm -t $IMAGE_NAME -f ./dockerfiles/$DOCKERFILEPATH .
-./c2w $IMAGENAME:latest $IMAGENAME.wasm
-sed -i 's/cl-bootcamp-base.wasm/$IMAGENAME.wasm/g' index.html
+./c2w $IMAGE_NAME:latest $IMAGE_FILE
+sed -i "s/cl-bootcamp-base.wasm/$IMAGE_FILE/g" index.html
